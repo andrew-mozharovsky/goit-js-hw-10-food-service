2c4785b0b028668771c4160f18c5fcc857327784
AndrewMozharovsky <71605765+AndrewMozharovsky@users.noreply.github.com>
2021-03-26 08:46:05 +0200

    local-fix

--- src/js/changeCheme.js
+++ src/js/changeCheme.js
@@ -9,11 +9,11 @@ const changeCheme = () => {
   if (refs.documentThem.classList.contains(themeColor.DARK)) {
     refs.documentThem.classList.remove(themeColor.DARK);
     refs.documentThem.classList.add(themeColor.LIGHT);
-    localStorage.setItem('themColor', refs.documentThem.classList.value);
+    localStorage.setItem('themColor', themeColor.LIGHT);
   } else {
     refs.documentThem.classList.remove(themeColor.LIGHT);
     refs.documentThem.classList.add(themeColor.DARK);
-    localStorage.setItem('themColor', refs.documentThem.classList.value);
+    localStorage.setItem('themColor', themeColor.DARK);
   }
 };
 
